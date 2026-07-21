a7d462f3b4d55a8ff32a1bbaafe39ec7260b5684
Phanindra Pullela <127200872+phanindra-dot@users.noreply.github.com>
2026-07-21 22:30:58 +0530

    Update audiofileload.py

--- audiofileload.py
+++ audiofileload.py
@@ -19,16 +19,12 @@ def load_librosa(path):
 
 
 def load_sound_file(path):
-   start=time.time()
-   print(start)
    y,sr=sf.read(path)
-   end=time.time()
-   print(end)
-   print(end-start)
    print(y.shape)
    print(sr)
    print(y)
    return y,sr
+   
 
 def load_torch_audio(path):
    y,sr=ta.load(path)
@@ -37,20 +33,10 @@ def load_torch_audio(path):
    print(y)
    return y,sr
 
-def load_stft(x):
-   X=torch.stft(x,n_fft=512,win_length=512,hop_length=256,return_complex=True)
-   return X
+
 
 if __name__ == "__main__":
 
    file_path=r"C:\Users\USER\Desktop\pytorch_basics\test.wav"
 
    audio_data,sample_rate=load_librosa(path=file_path)
-   #audio_data = audio_data.squeeze(0)
-   #print(type(audio_data))
-   #print(audio_data.shape)
-   #a=load_stft(audio_data)
-   #print(a)
-   #print(a.shape)
-
-   torch.stft()
